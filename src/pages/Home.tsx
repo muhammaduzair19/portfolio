8c64d6df677651d11517af48055a212d16068d22
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2024-05-03 20:07:54 +0500

    gsap remove

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,23 +4,9 @@ import code from '../assets/code.png'
 import Resume from '../assets/Resume.pdf'
 import { BiDownload } from "react-icons/bi";
 import WorkItem from "../components/WorkItem";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 
 
 const Home = () => {
-
-    useGSAP(() => {
-        gsap.from('.an', {
-            opacity: 0,
-            y: -30,
-            delay: 0.4,
-            duration: 0.8,
-            stagger: 0.3,
-            ease: 'back.inOut'
-        })
-    })
-
     return (
         <div className="w-full bg-bgdark-0">
             <section className="w-full mx-auto h-screen flex justify-center items-center bg-bgdark-0">
@@ -29,8 +15,7 @@ const Home = () => {
                         <p className="uppercase">hey, my name is <span className="text-white font-bold">uzair</span></p>
                         <p className="text-3xl font-bold md:text-3xl leading-tight tracking-tighter" >
                             <span className="text-white">
-                                <span className="an">React</span> <span className="an">JS</span> <span className="an">Developer</span> <span className="an">and</span> <span className="an">
-                                    Javascript</span> enthusiast from Pakistan.
+                                React JS Developer and Javascript enthusiast from Pakistan.
                             </span> I'm interested in learning new things about React, React Native, Node, and Typescript.
                         </p>
                         <div className="flex gap-4">
@@ -90,7 +75,7 @@ const Home = () => {
                     <h2 className="uppercase text-white font-bold text-2xl">work</h2>
                     <WorkItem
                         company="Tassaract"
-                        position='Jr. React Js Developer'
+                        position='Jr. React Js Developer'   
                         start='10/2023'
                         end='Current'
                     />
